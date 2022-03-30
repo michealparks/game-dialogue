ad05eb7b67a086fe76e3b29ec6218d5ab9efc920
Micheal Parks <michealparks1989@gmail.com>
2022-03-29 23:52:30 -0400

    publishing bugfixes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 
-declare module "*.yaml" {
+declare module '*.yaml' {
   const value: {
     bot: any
     dialogue: any
     variables: any
-  };
+  }
   export = value;
 }
