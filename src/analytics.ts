1337ab60b9c483b782104544468ff726cecf1b3d
Micheal Parks <michealparks1989@gmail.com>
2022-04-03 18:38:37 -0400

    dynamically import analytics

---
+++ src/analytics.ts
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/browser'
+import { BrowserTracing } from '@sentry/tracing'
+
+Sentry.init({
+  dsn: 'https://37267ffdc29e46899c67a4ad442cf267@o1187721.ingest.sentry.io/6307556',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.8,
+})
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import * as Sentry from '@sentry/browser'
-import { BrowserTracing } from '@sentry/tracing'
 import './main.css'
 import App from './App.svelte'
 
@@ -7,12 +5,4 @@ export default new App({
   target: document.querySelector('#app')!,
 })
 
-Sentry.init({
-  dsn: 'https://37267ffdc29e46899c67a4ad442cf267@o1187721.ingest.sentry.io/6307556',
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 0.8,
-})
+import('./analytics')
